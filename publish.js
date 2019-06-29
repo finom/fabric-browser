69969b5016f7e96dcf45ad20ca7d33e49700233c
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2019-06-29 18:51:17 +0300

    Some fixes at publish script

--- publish.js
+++ publish.js
@@ -6,19 +6,22 @@ execSync('git clone git@github.com:fabricjs/fabric.js.git fabric-repository');
 
 const pkgPath = path.resolve(__dirname, 'fabric-repository/package.json');
 
+const { name, description, version } = require('./package.json');
+
 writeFileSync(
   pkgPath,
   JSON.stringify({
     ...JSON.parse(
       readFileSync(pkgPath)
     ),
-    version: '0.0.1',
     optionalDependencies: {},
-    name: 'fabric-pure-browser',
-    description: require('./package.json').description,
+    version: `${version}-browser`,
+    name,
+    description,
   }, null, '\t'),
 );
 
-execSync('cd fabric-repository && npm i --no-shrinkwrap && npm run build && npm publish');
+execSync('cd fabric-repository && npm i --no-shrinkwrap && npm run build');
+execSync('npm publish fabric-repository');
 
 console.log('Success!');
